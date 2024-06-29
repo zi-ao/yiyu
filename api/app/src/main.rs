7675206c7e999e485bdeb429ec69867ff4375cda
zi-ao <869822845@qq.com>
2024-06-29 22:42:52 +0800

    修改 api 项目

---
+++ api/app/src/main.rs
@@ -0,0 +1,56 @@
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde_json::Value;
+use tracing_appender::{non_blocking, rolling};
+use tracing_subscriber::{
+    filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt, Registry,
+};
+use yiyu_core::logger;
+
+#[tokio::main]
+async fn main() {
+    // 初始化 tracing
+
+    // 环境筛选
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("debug"));
+    // 自定义的时间格式器
+    let custom_timer = logger::timer("[year]-[month]-[day] [hour]:[minute]:[second]");
+
+    // 输出文本到控制台中
+    let formatting_layer = logger::text_layer()
+        .with_writer(std::io::stderr);
+    // 输出JSON到文件中
+    let file_appender = rolling::never("logs", "app.log");
+    let (non_blocking_appender, _guard) = non_blocking(file_appender);
+    let file_layer = logger::json_layer()
+        .with_timer(custom_timer)
+        .with_writer(non_blocking_appender);
+
+    // 注册
+    Registry::default()
+        .with(env_filter)
+        .with(formatting_layer)
+        .with(file_layer)
+        .init();
+
+    // 构建应用路由
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // 使用 hyper 运行应用, 监听全局端口 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+// 根路由
+pub async fn root() -> (StatusCode, Json<Value>) {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "version": "0.1.0",
+            "message": "一隅之地，偏安于此。"
+        }"#;
+
+    (StatusCode::OK, Json(serde_json::from_str(data).unwrap()))
+}
---
+++ api/yiyu-core/src/logger.rs
@@ -0,0 +1,36 @@
+use time::{format_description, UtcOffset};
+use tracing_subscriber::fmt::{
+    format::{Format, Json, JsonFields},
+    layer as fmt_layer,
+    time::OffsetTime,
+    Layer,
+};
+use tracing_subscriber::fmt::format::Pretty;
+
+/// 创建自定义的时间格式器
+///
+/// timer("[year]-[month]-[day] [hour]:[minute]:[second]");
+pub fn timer(s: &str) -> OffsetTime<Vec<format_description::BorrowedFormatItem>> {
+    // 定义自定义时间格式
+    let custom_format = format_description::parse(s).expect("Invalid time format");
+    // 获取当前时区偏移量
+    let time_offset = UtcOffset::current_local_offset().unwrap_or_else(|_| UtcOffset::UTC);
+
+    OffsetTime::new(time_offset, custom_format)
+}
+
+pub fn text_layer<S>() -> Layer<S, Pretty, Format<Pretty>> {
+	fmt_layer().pretty()
+}
+
+/// 拥有 JSON 格式器的 Layer
+///
+/// json_layer();
+pub fn json_layer<S>() -> Layer<S, JsonFields, Format<Json>> {
+    fmt_layer()
+        .json()
+        .with_ansi(false)
+        .with_span_list(true)
+        .with_file(true)
+        .with_line_number(true)
+}
